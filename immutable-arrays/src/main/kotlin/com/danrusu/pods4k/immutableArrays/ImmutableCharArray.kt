a771c3dd64766db240f83cc6fff2bc922be51132
Dan Rusu <roosterdan@gmail.com>
2024-03-22 23:36:40 -0600

    Add generated ImmutableCharArray class along with its associated test

---
+++ immutable-arrays/src/main/kotlin/com/danrusu/pods4k/immutableArrays/ImmutableCharArray.kt
@@ -0,0 +1,46 @@
+// Auto-generated file. DO NOT EDIT!
+package com.danrusu.pods4k.immutableArrays
+
+import kotlin.Char
+import kotlin.CharArray
+import kotlin.Int
+import kotlin.PublishedApi
+import kotlin.collections.Iterator
+import kotlin.jvm.JvmInline
+
+@JvmInline
+public value class ImmutableCharArray @PublishedApi internal constructor(
+  private val values: CharArray,
+) {
+  public val size: Int
+    get() = values.size
+
+  /**
+   * Returns the element at the specified [index]. This method can be called using the index
+   * operator.
+   */
+  public operator fun `get`(index: Int): Char = values[index]
+
+  /**
+   * Creates an iterator allowing iteration over the elements of the array.
+   */
+  public operator fun iterator(): Iterator<Char> = values.iterator()
+
+  public companion object {
+    /**
+     * Creates a ImmutableCharArray instance of the specified [size], where each element is
+     * calculated by calling the specified [init] function.
+     *
+     * [init] is called sequentially starting at index 0 to initialize the array with each element
+     * at its given index.
+     *
+     * Implementation:
+     * We're using the invoke method instead of a regular constructor so that we can declare it
+     * inline.  The call site will look like a regular constructor call.
+     */
+    public inline operator fun invoke(size: Int, `init`: (index: Int) -> Char): ImmutableCharArray {
+      val backingArray = CharArray(size) { index -> init(index) }
+      return ImmutableCharArray(backingArray)
+    }
+  }
+}
---
+++ immutable-arrays/src/test/kotlin/com/danrusu/pods4k/immutableArrays/ImmutableCharArrayTest.kt
@@ -0,0 +1,41 @@
+package com.danrusu.pods4k.immutableArrays
+
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+
+private val primitiveCharClass = 'a'::class.java
+
+/**
+ * IMPORTANT:
+ * Read TESTING-STRATEGY.md before modifying this file.
+ */
+class ImmutableCharArrayTest {
+    @Test
+    fun `ensure that primitive class variable is correct`() {
+        expectThat(primitiveCharClass.toString()).isEqualTo("char")
+    }
+
+    @Test
+    fun `creation validation`() {
+        expectThat(ImmutableCharArray(1) { it.toChar() })
+            .isA<ImmutableCharArray>()
+    }
+
+    @Test
+    fun `get validation`() {
+        with(ImmutableCharArray(1) { it.toChar() }) {
+            expectThat(get(0)::class.java).isEqualTo(primitiveCharClass)
+        }
+    }
+
+    @Test
+    fun `iterator validation`() {
+        with(ImmutableCharArray(3) { it.toChar() }) {
+            for (value in this) {
+                expectThat(value::class.java).isEqualTo(primitiveCharClass)
+            }
+        }
+    }
+}
