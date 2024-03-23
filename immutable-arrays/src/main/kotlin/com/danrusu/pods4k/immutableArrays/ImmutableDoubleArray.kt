1816d21fd7895f62ea42ae8ceb748f657a23fd43
Dan Rusu <roosterdan@gmail.com>
2024-03-22 23:36:58 -0600

    Add generated ImmutableDoubleArray class along with its associated test

---
+++ immutable-arrays/src/main/kotlin/com/danrusu/pods4k/immutableArrays/ImmutableDoubleArray.kt
@@ -0,0 +1,47 @@
+// Auto-generated file. DO NOT EDIT!
+package com.danrusu.pods4k.immutableArrays
+
+import kotlin.Double
+import kotlin.DoubleArray
+import kotlin.Int
+import kotlin.PublishedApi
+import kotlin.collections.Iterator
+import kotlin.jvm.JvmInline
+
+@JvmInline
+public value class ImmutableDoubleArray @PublishedApi internal constructor(
+  private val values: DoubleArray,
+) {
+  public val size: Int
+    get() = values.size
+
+  /**
+   * Returns the element at the specified [index]. This method can be called using the index
+   * operator.
+   */
+  public operator fun `get`(index: Int): Double = values[index]
+
+  /**
+   * Creates an iterator allowing iteration over the elements of the array.
+   */
+  public operator fun iterator(): Iterator<Double> = values.iterator()
+
+  public companion object {
+    /**
+     * Creates a ImmutableDoubleArray instance of the specified [size], where each element is
+     * calculated by calling the specified [init] function.
+     *
+     * [init] is called sequentially starting at index 0 to initialize the array with each element
+     * at its given index.
+     *
+     * Implementation:
+     * We're using the invoke method instead of a regular constructor so that we can declare it
+     * inline.  The call site will look like a regular constructor call.
+     */
+    public inline operator fun invoke(size: Int, `init`: (index: Int) -> Double):
+        ImmutableDoubleArray {
+      val backingArray = DoubleArray(size) { index -> init(index) }
+      return ImmutableDoubleArray(backingArray)
+    }
+  }
+}
---
+++ immutable-arrays/src/test/kotlin/com/danrusu/pods4k/immutableArrays/ImmutableDoubleArrayTest.kt
@@ -0,0 +1,41 @@
+package com.danrusu.pods4k.immutableArrays
+
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+
+private val primitiveDoubleClass = 3.0::class.java
+
+/**
+ * IMPORTANT:
+ * Read TESTING-STRATEGY.md before modifying this file.
+ */
+class ImmutableDoubleArrayTest {
+    @Test
+    fun `ensure that primitive class variable is correct`() {
+        expectThat(primitiveDoubleClass.toString()).isEqualTo("double")
+    }
+
+    @Test
+    fun `creation validation`() {
+        expectThat(ImmutableDoubleArray(1) { it.toDouble() })
+            .isA<ImmutableDoubleArray>()
+    }
+
+    @Test
+    fun `get validation`() {
+        with(ImmutableDoubleArray(1) { it.toDouble() }) {
+            expectThat(get(0)::class.java).isEqualTo(primitiveDoubleClass)
+        }
+    }
+
+    @Test
+    fun `iterator validation`() {
+        with(ImmutableDoubleArray(3) { it.toDouble() }) {
+            for (value in this) {
+                expectThat(value::class.java).isEqualTo(primitiveDoubleClass)
+            }
+        }
+    }
+}
