5a98506083ea521e6d2e3ac6280c50680d12c9b8
Dan Rusu <roosterdan@gmail.com>
2024-03-22 23:36:28 -0600

    Add generated ImmutableByteArray class along with its associated test

---
+++ immutable-arrays/src/test/kotlin/com/danrusu/pods4k/immutableArrays/ImmutableByteArrayTest.kt
@@ -0,0 +1,41 @@
+package com.danrusu.pods4k.immutableArrays
+
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+
+private val primitiveByteClass = 3.toByte()::class.java
+
+/**
+ * IMPORTANT:
+ * Read TESTING-STRATEGY.md before modifying this file.
+ */
+class ImmutableByteArrayTest {
+    @Test
+    fun `ensure that primitive class variable is correct`() {
+        expectThat(primitiveByteClass.toString()).isEqualTo("byte")
+    }
+
+    @Test
+    fun `creation validation`() {
+        expectThat(ImmutableByteArray(1) { it.toByte() })
+            .isA<ImmutableByteArray>()
+    }
+
+    @Test
+    fun `get validation`() {
+        with(ImmutableByteArray(1) { it.toByte() }) {
+            expectThat(get(0)::class.java).isEqualTo(primitiveByteClass)
+        }
+    }
+
+    @Test
+    fun `iterator validation`() {
+        with(ImmutableByteArray(3) { it.toByte() }) {
+            for (value in this) {
+                expectThat(value::class.java).isEqualTo(primitiveByteClass)
+            }
+        }
+    }
+}
