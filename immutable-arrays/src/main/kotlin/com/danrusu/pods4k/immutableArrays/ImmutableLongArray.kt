bfff2bb238e1bfb934beab2f182cb0c14807c7a5
Dan Rusu <roosterdan@gmail.com>
2024-03-22 23:37:25 -0600

    Add generated ImmutableLongArray class along with its associated test

---
+++ immutable-arrays/src/main/kotlin/com/danrusu/pods4k/immutableArrays/ImmutableLongArray.kt
@@ -0,0 +1,46 @@
+// Auto-generated file. DO NOT EDIT!
+package com.danrusu.pods4k.immutableArrays
+
+import kotlin.Int
+import kotlin.Long
+import kotlin.LongArray
+import kotlin.PublishedApi
+import kotlin.collections.Iterator
+import kotlin.jvm.JvmInline
+
+@JvmInline
+public value class ImmutableLongArray @PublishedApi internal constructor(
+  private val values: LongArray,
+) {
+  public val size: Int
+    get() = values.size
+
+  /**
+   * Returns the element at the specified [index]. This method can be called using the index
+   * operator.
+   */
+  public operator fun `get`(index: Int): Long = values[index]
+
+  /**
+   * Creates an iterator allowing iteration over the elements of the array.
+   */
+  public operator fun iterator(): Iterator<Long> = values.iterator()
+
+  public companion object {
+    /**
+     * Creates a ImmutableLongArray instance of the specified [size], where each element is
+     * calculated by calling the specified [init] function.
+     *
+     * [init] is called sequentially starting at index 0 to initialize the array with each element
+     * at its given index.
+     *
+     * Implementation:
+     * We're using the invoke method instead of a regular constructor so that we can declare it
+     * inline.  The call site will look like a regular constructor call.
+     */
+    public inline operator fun invoke(size: Int, `init`: (index: Int) -> Long): ImmutableLongArray {
+      val backingArray = LongArray(size) { index -> init(index) }
+      return ImmutableLongArray(backingArray)
+    }
+  }
+}
---
+++ immutable-arrays/src/test/kotlin/com/danrusu/pods4k/immutableArrays/ImmutableLongArrayTest.kt
@@ -0,0 +1,41 @@
+package com.danrusu.pods4k.immutableArrays
+
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+
+private val primitiveLongClass = 3L::class.java
+
+/**
+ * IMPORTANT:
+ * Read TESTING-STRATEGY.md before modifying this file.
+ */
+class ImmutableLongArrayTest {
+    @Test
+    fun `ensure that primitive class variable is correct`() {
+        expectThat(primitiveLongClass.toString()).isEqualTo("long")
+    }
+
+    @Test
+    fun `creation validation`() {
+        expectThat(ImmutableLongArray(1) { it.toLong() })
+            .isA<ImmutableLongArray>()
+    }
+
+    @Test
+    fun `get validation`() {
+        with(ImmutableLongArray(1) { it.toLong() }) {
+            expectThat(get(0)::class.java).isEqualTo(primitiveLongClass)
+        }
+    }
+
+    @Test
+    fun `iterator validation`() {
+        with(ImmutableLongArray(3) { it.toLong() }) {
+            for (value in this) {
+                expectThat(value::class.java).isEqualTo(primitiveLongClass)
+            }
+        }
+    }
+}
